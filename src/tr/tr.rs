4e3cb77a37d1545101ce00a665925c1c2aa77d59
Hrittik <impulsiv.rekt.mind@gmail.com>
2020-07-02 09:51:31 +0530

    added traits

--- src/tr/tr.rs
+++ src/tr/tr.rs
@@ -2,6 +2,8 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use std::fmt::Debug;
+
 trait Animal {
     // a function that accepts a string
     fn create(name: &'static str) -> Self;
@@ -51,6 +53,67 @@ impl Animal for Cat {
     }
 }
 
+// --------------------------------------------------
+// we are not manually writing the debug trait
+// instead we are using std's Debug trait ans asking the compiler to derive it from there
+#[derive(Debug)]
+struct Circle {
+    radius: f64,
+}
+
+#[derive(Debug)]
+struct Square {
+    side: f64,
+}
+
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+impl Shape for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        self.radius * self.radius * std::f64::consts::PI
+    }
+}
+
+// takes a trait as a param
+fn print_info(shape: impl Shape) {
+    println!("area = {}", shape.area());
+}
+
+// takes a trait as a param
+// we use + when the arg (here, shape) needs to be able to implement multiple traits
+// there are three syntax ways for writing this
+
+// syntax 1:
+/* fn print_more_info(shape: impl Shape + Debug) { */
+// syntax 2:
+/* fn print_more_info<T: Shape + Debug>(shape: T) { */
+// syntax 3:
+fn print_more_info<T>(shape: T) where T: Shape + Debug {
+    println!("{:?}", shape);
+    println!("area = {}", shape.area());
+}
+
+fn trait_parameters() {
+    let c = Circle { radius: 2.0 };
+    /* print_info(c); */
+    print_more_info(c);
+    let s = Square { side: 6.0 };
+}
+
+// --------------------------------------------------
+fn into_demo() {
+    
+}
+
+// --------------------------------------------------
 pub fn traits() {
     /* let h1 = Human { name:"John" }; */
     /* let h1 = Human::create("John"); */
@@ -60,4 +123,11 @@ pub fn traits() {
     /* let c1 = Cat { name:"Bitch" }; */
     let c1 = Cat::create("Bitch");
     c1.talk();
+
+    // TRAIT PARAMETERS:
+    // demoing functions that take traits as params
+    trait_parameters();
+
+    // INTO:
+    into_demo();
 }
